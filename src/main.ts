0e8c14a57df52b08db419282e440de2f37335285
altafkorejo17 <altafkorejo17@gmail.com>
2024-12-03 06:00:22 +0400

    Starting Up a next project

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import {Controller, Module, Get} from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 
 @Controller()
 class AppController {
@@ -8,3 +9,16 @@ class AppController {
         return "Hi there...";
     }
 }
+
+@Module({
+    controllers: [AppController],
+})
+export class AppModule {}
+
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    app.listen(3000);
+}
+
+bootstrap();
